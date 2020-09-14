f88be5462ee9865cacd041af2b10630eda32f6c5
Luis Capuyon <frostehhh967@gmail.com>
2020-09-14 18:08:53 +0800

    Added React

---
+++ React/src/App.js
@@ -0,0 +1,69 @@
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import React from 'react';
+import DataGrid, { Editing, Column, Lookup } from 'devextreme-react/data-grid';
+import './App.css';
+import service from './data'
+import MultipleDropDownBox from './components/MultipleDropDownBox';
+
+const dataSource = service.getEmployees(),
+  states = service.getStates(),
+  cities = service.getCities();
+
+function App() {
+  function renderMultipleDropDownBox(value, setValue, dataSource) {
+    return <MultipleDropDownBox dataSource={dataSource} value={value} setValue={setValue}/>
+  }
+
+  return (
+    <div className="App">
+      <DataGrid dataSource={dataSource}>
+          <Editing
+            mode="row"
+            allowUpdating={true}
+            allowAdding={true}>
+          </Editing>
+          <Column 
+            dataField="StateID" 
+            caption="State" 
+            setCellValue={setStateValue}
+            cellTemplate={arrayCellTemplate} 
+            editCellRender={({value, setValue}) => renderMultipleDropDownBox(value, setValue, states)}>
+            <Lookup dataSource={states} displayExpr="Name" valueExpr="ID" />
+          </Column>
+          <Column 
+            dataField="CityID" 
+            caption="City" 
+            cellTemplate={arrayCellTemplate}
+            editCellRender={({value, setValue}) => renderMultipleDropDownBox(value, setValue, cities)}>
+            <Lookup dataSource={getFilteredCities} displayExpr="Name" valueExpr="ID" />
+          </Column>
+        </DataGrid>
+    </div>
+  );
+}
+
+function getFilteredCities(options) {
+  return {
+    store: cities,
+    filter: options.data ? ["StateID", "=", options.data.StateID] : null
+  };
+}
+
+function setStateValue(rowData, value) {
+  rowData.CityID = [];
+  this.defaultSetCellValue(rowData, value);
+}
+
+function arrayCellTemplate(container, options) {
+  var noBreakSpace = "\u00A0",
+    text = (options.value || [])
+      .map(element => {
+        return options.column.lookup.calculateCellValue(element);
+      })
+      .join(", ");
+  container.textContent = text || noBreakSpace;
+  container.title = text;
+}
+
+export default App;
---
+++ React/src/components/MultipleDropDownBox.js
@@ -0,0 +1,62 @@
+import React, { useReducer } from 'react';
+
+import DataGrid, { Column, Selection } from 'devextreme-react/data-grid';
+import DropDownBox from 'devextreme-react/drop-down-box';
+import DropDownBoxSave from './DropDownBoxSave';
+
+export default function MultipleDropDownBox({value, dataSource, setValue}) {
+    const [{selectedKeys, currentValue, isOpened}, dispatch] = useReducer(reducer, {
+        selectedKeys: value, 
+        currentValue: value,
+        isOpened: false
+    })
+    const dropDownOpts = {
+        onShowing: () => {
+            dispatch({type: "openDropdown"})
+        },
+        visible: isOpened
+    } 
+
+    function onSelectionChanged({selectedRowKeys}) {
+        dispatch({type: "selectedKeys", data: selectedRowKeys})
+    }
+
+    function renderDataGrid({component}) {
+        return (
+            <React.Fragment>
+                <DataGrid 
+                    dataSource={dataSource} 
+                    keyExpr="ID"
+                    selectedRowKeys={selectedKeys}
+                    hoverStateEnabled={true}
+                    height={250}
+                    onSelectionChanged={onSelectionChanged}> 
+                    <Column dataField="ID" />
+                    <Column dataField="Name" />
+                    <Selection mode="multiple" />
+                </DataGrid>
+                <DropDownBoxSave selectedKeys={selectedKeys} setCellValue={setValue} setState={dispatch} dropDownBoxClose={component.close}/>
+            </React.Fragment>
+        )
+    }
+
+    return (
+        <DropDownBox 
+            dataSource={dataSource}
+            value={currentValue}
+            displayExpr="Name"
+            valueExpr="ID"
+            dropDownOptions={dropDownOpts}
+            contentRender={renderDataGrid}
+        />
+    )
+}
+
+function reducer(state, action) {
+    if(action.type === "dropDownBoxValue") {
+        return {...state, currentValue: action.data, isOpened: false}
+    } else if(action.type === "selectedKeys")
+        return {...state, selectedKeys: action.data}
+    else if(action.type === "openDropdown") 
+        return {...state, isOpened: true}
+}
